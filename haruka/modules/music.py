94a18cc3e2c4b6de079535d81b869f148dfcd3d9
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-26 19:11:40 +0530

    Update music.py

--- haruka/modules/music.py
+++ haruka/modules/music.py
@@ -1,5 +1,3 @@
-"""do `.song <song keyword>`"""
-
 from telethon import events
 import subprocess
 from telethon.errors import MessageEmptyError, MessageTooLongError, MessageNotModifiedError
@@ -10,8 +8,6 @@ from haruka.events import register
 import glob
 import os
 import instantmusic,subprocess
-os.system("rm -rf *.mp3")
-    
 
 @register(pattern="^/song (.*)")
 async def _(event):
@@ -21,7 +17,6 @@ async def _(event):
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
-    await event.reply("ok finding the song")
     os.system("instantmusic -q -s "+cmd)
     bro = 'for f in *.webm; do      mv -- "$f" "${f%.webm}.mp3"; done'
     os.system(bro)
@@ -29,7 +24,7 @@ async def _(event):
     loa = l[0]
     await event.reply("sending the song")
     await event.client.send_file(
-                event.chat_id,
+                eventpp0.chat_id,
                 loa,
                 force_document=True,
                 allow_cache=False,
@@ -37,7 +32,6 @@ async def _(event):
                 reply_to=reply_to_id
             )
     os.system("rm -rf *.mp3")
-    subprocess.check_output("rm -rf *.mp3",shell=True)
 
 __help__ = """
  - /song <name>: search download and return a song in the best format
