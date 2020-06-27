3a5fe177157b17886ea1de67156e8a93e4ef9261
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-27 21:54:12 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -693,7 +693,7 @@ async def _(event):
         url = result.get("url")
         description = result.get("description")
         last = html2text.html2text(description)
-        output_str += "[{}]({})\n`{}`\n\n".format(text, url, last)       
+        output_str += "[{}]({})\n{}\n\n".format(text, url, last)       
     await event.reply("{}".format(output_str), link_preview=False, parse_mode='Markdown')
 
 import aiohttp
