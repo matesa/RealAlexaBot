eb799d3ae19282549769301730845aa14d5c4b74
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-06-27 23:07:46 +0530

    Update assistant.py

--- haruka/modules/assistant.py
+++ haruka/modules/assistant.py
@@ -1,5 +1,8 @@
 __help__ = """
+*For text assistant*
  - /alexa <question>: Ask alexa any question and it will give accurate reply. For eg: `/alexa where is Taj Mahal`, `/alexa what is the age of Virat Kohli` etc..
+*For voice assistant*
+ - /alexa: Reply to a voice query and get the results in voice output
 
 *NOTE*
 The question should be a meaningful one otherwise you will get no response !
