0a70ca0912b2fe318d258efcaa9ea08771f65e3d
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-01 10:29:34 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1692,79 +1692,9 @@ async def chat_bot_update(ebent):
                 await ebent.reply(text_rep)
              except (KeyError, TypeError):
                 return
-   if ebent.media:
-      if ebent.reply_to_msg_id:
-        previous_message = await ebent.get_reply_message()
-        required_file_name = await ebent.client.download_media(previous_message, TEMP_DOWNLOAD_DIRECTORY)
-        if IBM_WATSON_CRED_URL is None or IBM_WATSON_CRED_PASSWORD is None:
-            await ebent.reply("You need to set the required ENV variables for this module. \nModule stopping")
-        else:
-            headers = {
-                "Content-Type": previous_message.media.document.mime_type,
-            }
-            data = open(required_file_name, "rb").read()
-            response = requests.post(
-                IBM_WATSON_CRED_URL + "/v1/recognize",
-                headers=headers,
-                data=data,
-                auth=("apikey", IBM_WATSON_CRED_PASSWORD)
-            )
-            r = response.json()
-            if "results" in r:
-                # process the json to appropriate string format
-                results = r["results"]
-                transcript_response = ""
-                transcript_confidence = ""
-                for alternative in results:
-                    alternatives = alternative["alternatives"][0]
-                    transcript_response += " " + str(alternatives["transcript"]) 
-                if transcript_response != "":
-                    string_to_show = "{}".format(transcript_response)           
-                    auto_chats = auto_chat.find({})
-                    for ch in auto_chats:
-                      if ebent.chat_id == ch['id'] and ebent.from_id == ch['user']:
-                         try:
-                            msg = str(ebent.text)
-                            session = lydia.create_session()
-                            session_id = session.id
-                            text_rep = session.think_thought(msg)
-                            answer = text_rep
-                         except (KeyError, TypeError):
-                            return
-                         try:
-                          tts = gTTS(answer, tld='com', lang='en')
-                          tts.save("results.mp3")
-                         except AssertionError: 
-                          return
-                         except ValueError:    
-                          return
-                         except RuntimeError:        
-                          return
-                         except gTTSError:
-                          return
-                         with open("results.mp3", "r"):
-                          await ebent.client.send_file(ebent.chat_id, "results.mp3", voice_note=True, reply_to=ebent.id)
-                         os.remove("results.mp3")
-                else:                   
-                    try:
-                       answer = "Sorry I can't recognise your query"
-                       tts = gTTS(answer, tld='com', lang='en')
-                       tts.save("results.mp3")
-                    except AssertionError: 
-                      return
-                    except ValueError:    
-                      return
-                    except RuntimeError:        
-                      return
-                    except gTTSError:
-                      return
-                    with open("results.mp3", "r"):
-                        await ebent.client.send_file(ebent.chat_id, "results.mp3", voice_note=True, reply_to=ebent.id)
-                    os.remove("results.mp3")
-            else:
-                await ebent.reply("API Failure !")
-                os.remove(required_file_name)
-
+   if not ebent.text:
+      return
+      
 import os
 import sys
 
