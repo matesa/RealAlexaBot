5bc756182a6b99079106f19e1560780ce6ca80b9
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-02 10:18:19 +0530

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -97,20 +97,19 @@ if ENV:
     SPAMMERS =os.environ.get('SPAMMERS', "")
     
     def spamfilters(text, user_id, chat_id, message):
-	# If msg from self, return True
-	if user_id == 1002584093:
-		return False
-	print("{} | {} | {} | {}".format(text, user_id, message.chat.title, chat_id))
-	if antispam_module:
-		parsing_date = time.mktime(message.date.timetuple())
-		detecting = detect_user(user_id, chat_id, message, parsing_date)
-		if detecting:
-			return True
-		antispam_restrict_user(user_id, parsing_date)
-	if int(user_id) in SPAMMERS:
-		print("This user is spammer!")
-		return True
-	else:
-		return False
+        if user_id == 1002584093:
+           return False
+        print("{} | {} | {} | {}".format(text, user_id, message.chat.title, chat_id))
+        if antispam_module:
+           parsing_date = time.mktime(message.date.timetuple())
+        detecting = detect_user(user_id, chat_id, message, parsing_date)
+        if detecting:
+           return True
+        antispam_restrict_user(user_id, parsing_date)
+        if int(user_id) in SPAMMERS:
+           print("This user is spammer!")
+           return True
+        else:
+           return False
 else:
    quit(1)
