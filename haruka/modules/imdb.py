fcb33704b3a90ddc100ad36fea4f7b754531b557
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-05 14:57:59 +0530

    Update imdb.py

--- haruka/modules/imdb.py
+++ haruka/modules/imdb.py
@@ -1,12 +1,3 @@
-
-# For UniBorg
-# Copyright (c) JeepBot | 2019 
-# (c) JeepBot is not occur to all modules in here
-"""
-Imdb Module
-.imdb
-"""
-
 import bs4
 import requests
 import asyncio
@@ -19,9 +10,30 @@ from haruka.events import register
 
 langi = "en"
 
-#kanged from Blank-x ;---;
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await tbot(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
+        )
+    elif isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id))) \
+            .full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
+        )
+    else:
+        return None
+
 @register(pattern="^/imdb (.*)") 
 async def imdb(e):
+ if not (await is_register_admin(e.input_chat, e.message.sender_id)):
+       await e.reply("I only respond to admins so go get some permissions !")
+       return
  try:
     movie_name = e.pattern_match.group(1)
     remove_space = movie_name.split(' ')
