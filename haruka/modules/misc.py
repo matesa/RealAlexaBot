6c14d4c97435601c326de8c6e774b84082d50b46
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-05 14:01:08 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -264,13 +264,13 @@ async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
 
         return isinstance(
-            (await bot(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (await tbot(functions.channels.GetParticipantRequest(chat, user))).participant,
             (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
         )
     elif isinstance(chat, types.InputPeerChat):
 
-        ui = await bot.get_peer_id(user)
-        ps = (await bot(functions.messages.GetFullChatRequest(chat.chat_id))) \
+        ui = await tbot.get_peer_id(user)
+        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id))) \
             .full_chat.participants.participants
         return isinstance(
             next((p for p in ps if p.user_id == ui), None),
