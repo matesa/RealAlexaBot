b0831f0fcfeab3de3745b9a408ceee9e1a3125ae
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-05 21:42:44 +0530

    Update zip.py

--- haruka/modules/zip.py
+++ haruka/modules/zip.py
@@ -40,7 +40,6 @@ async def _(event):
         return 
     if event.is_group:
      if not (await is_register_admin(event.input_chat, event.message.sender_id)):
-       await event.reply("I only respond to admins so go get some permissions !")
        return
    
     mone = await event.reply("Processing ...")
