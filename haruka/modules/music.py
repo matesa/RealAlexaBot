c834a9ebefb56622364da8d02d30fd8988bb4046
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-05 14:59:25 +0530

    Update music.py

--- haruka/modules/music.py
+++ haruka/modules/music.py
@@ -9,10 +9,32 @@ import glob
 import os
 import spotdl, subprocess
 
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (await tbot(functions.channels.GetParticipantRequest(chat, user))).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator)
+        )
+    elif isinstance(chat, types.InputPeerChat):
+
+        ui = await tbot.get_peer_id(user)
+        ps = (await tbot(functions.messages.GetFullChatRequest(chat.chat_id))) \
+            .full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator)
+        )
+    else:
+        return None
+
 @register(pattern="^/song (.*)")
 async def _(event):
     if event.fwd_from:
         return
+    if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+       await event.reply("I only respond to admins so go get some permissions !")
+       return
     cmd = event.pattern_match.group(1)
     cmnd = f'"{cmd}"'
     reply_to_id = event.message.id
