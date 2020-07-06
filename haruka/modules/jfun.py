0320e65a0ad356f428898a7f25769078974bf675
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-06 11:01:02 +0530

    Delete jfun.py

--- haruka/modules/jfun.py
+++
@@ -1,9 +0,0 @@
-__help__ = """
- - /cowsay | /tuxsay | /milksay | /kisssay | /wwwsay | /defaultsay | /bunnysay | /moosesay | /sheepsay | /rensay | /cheesesay | /ghostbusterssay | /skeletonsay <text>: Returns a stylish art text from the given text	
- - /deepfry: Type this in reply to an image/sticker to roast the image/sticker	
- - /figlet: Another Style art	
- - /dice: Roll A dice	
- - /dart: Throw a dart and try your luck
- - /basketball: Try your luck if you can enter the ball in the ring
-"""
-__mod_name__ = "Just4Fun"
