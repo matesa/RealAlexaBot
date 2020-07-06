748e65e87dc6a9612d2972aa3103939562b5bec3
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-06 18:06:33 +0530

    Delete github.py

--- haruka/modules/github.py
+++
@@ -1,5 +0,0 @@
-__help__ = """
- - /git <username>: Returns info about a GitHub user or organization.
- - /repo <username>: Return the GitHub user or organization repository list 
-"""
-__mod_name__ = "Github"
