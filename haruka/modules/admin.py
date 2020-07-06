3b042f39189cea0a7edbf7799c13e2af572c4315
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-06 10:51:39 +0530

    Update admin.py

--- haruka/modules/admin.py
+++ haruka/modules/admin.py
@@ -281,6 +281,9 @@ __help__ = """
  - /invitelink: gets invitelink
  - /promote: promotes the user replied to
  - /demote: demotes the user replied to
+ - /zombies: Searches the number of deleted account in your group 
+ - /zombies clean: Removes all deleted account from your group 
+ - /kickthefools: Kicks all members who are inactive since 1 week
 """
 
 __mod_name__ = "Admin"
