582f9397632ce531c58b5133b045e4437dff9586
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-11 18:38:30 +0530

    Update memes.py

--- haruka/modules/memes.py
+++ haruka/modules/memes.py
@@ -297,6 +297,7 @@ Some memes command, find it all out yourself!
  - /getsticker: Uploads the .png of the sticker you've replied to
  - /kang: Reply to a sticker to add it to your pack or makes a new one if it doesn't exist
  - /sticklet <text>: Turn a text into a sticker, you'll get a random colour from a rainbow(out of 7 colours)
+ - /fortune: gets a random fortune quote
 """
 
 __mod_name__ = "Memes 💢"
