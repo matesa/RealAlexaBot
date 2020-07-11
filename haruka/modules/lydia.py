098d615e9d070553f460e1b20c6d9e324f755e18
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-11 20:53:23 +0530

    Update lydia.py

--- haruka/modules/lydia.py
+++ haruka/modules/lydia.py
@@ -1,6 +1,5 @@
 __help__ = """
  - /autochat: Type in reply to your message to activate. The bot will give auto replies to you\nCan also be used in PM's
  - /stopchat: Type in reply to your message to deactivate. The bot will stop giving auto replies to you
- - /smartchat: Activtes AI mode in the chat. The bot will talk to anyone who'll tag the bot 
 """
 __mod_name__ = "ChatBot 🤖"
