d14e5a1f3342d0ea34532fc43beb9bd8788f5e32
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-12 21:30:28 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -1876,6 +1876,63 @@ async def asciiart(event):
         os.remove(os.path.join(directory, item))
     
 
+import asyncio
+import barcode
+import os
+import time
+from barcode.writer import ImageWriter
+from datetime import datetime
+from haruka.events import register
+from telethon import events
+
+
+@register(pattern="^/barcode ?(.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    input_str = event.pattern_match.group(1)
+    message = "SYNTAX: `.barcode <long text to include>`"
+    reply_msg_id = event.message.id
+    if input_str:
+        message = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        reply_msg_id = previous_message.id
+        if previous_message.media:
+            downloaded_file_name = await event.client.download_media(
+                previous_message,
+                Config.TEMP_DOWNLOAD_DIRECTORY,
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            message = ""
+            for m in m_list:
+                message += m.decode("UTF-8") + "\r\n"
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+    else:
+        message = "SYNTAX: `.barcode <long text to include>`"
+    bar_code_type = "code128"
+    try:
+        bar_code_mode_f = barcode.get(bar_code_type, message, writer=ImageWriter())
+        filename = bar_code_mode_f.save(bar_code_type)
+        await event.client.send_file(
+            event.chat_id,
+            filename,
+            caption=message,
+            reply_to=reply_msg_id,
+        )
+        os.remove(filename)
+    except Exception as e:
+        await event.reply(str(e))
+        return
+    end = datetime.now()
+    ms = (end - start).seconds
+    await event.reply("Created BarCode in {} seconds".format(ms))
+
 
 __help__ = """
  - /id: get the current group id. If used by replying to a message, gets that user's id.
@@ -1934,6 +1991,7 @@ If you are still messed up send `/helptorrent` in pm for the tutorial !
  - /specs <brand> <device>: Get device specifications info
  - /twrp <codename>: Get the latest TWRP download for an Android device
  - /song <songname artist(optional)>: uploads the song in it's best quality available
+ - /barcode <text>: Makes a barcode out of the text
 """
 
 __mod_name__ = "Utilities ⚡"
