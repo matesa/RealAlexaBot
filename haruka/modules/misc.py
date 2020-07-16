3d45ae37439df6b5631ef5d294ef9b01c7d54a8d
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-07-16 19:29:55 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -2141,44 +2141,6 @@ def get_markdown(reply):
         markdown.append(md_item)
     return markdown
 
-"""QuotLy: Avaible commands: .qbot
-"""
-import datetime
-from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from haruka import ubot
-
-@register(pattern=r"^/qbot(?: |$)(.*)")
-async def _(event):
-    if event.fwd_from:
-        return 
-    if not event.reply_to_msg_id:
-       await event.reply("```Reply to any user message.```")
-       return
-    reply_message = await event.get_reply_message() 
-    if not reply_message.text:
-       await event.reply("```Reply to text message```")
-       return
-    chat = "@QuotLyBot"
-    sender = reply_message.sender
-    if reply_message.sender.bot:
-       await event.reply("```Reply to actual users message.```")
-       return
-    await event.reply("```Making a Quote```")
-    async with ubot.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
-              await ubot.send_message(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("```Please unblock @QuotLyBot and try again```")
-              return
-          if response.text.startswith("Hi!"):
-             await event.reply("```Can you kindly disable your forward privacy settings for good?```")
-          else:
-             await tbot.reply(event.chat_id, response.message)
-
 __help__ = """
  - /id: get the current group id. If used by replying to a message, gets that user's id.
  - /runs: reply a random string from an array of replies.
