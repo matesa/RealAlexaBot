ebf61b06f0fcc000a93a2f5a8f35e76720fdfdae
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-08-15 11:53:50 +0530

    Update __init__.py

--- haruka/__init__.py
+++ haruka/__init__.py
@@ -95,20 +95,11 @@ if ENV:
     except ModuleNotFoundError:
       antispam_module = False
       
-    def spamfilters(text, user_id, chat_id, message):
-        if user_id == 1372161900:
-           return False
-        print("{} | {} | {} | {}".format(text, user_id, message.chat.title, chat_id))
-        if antispam_module:
-           parsing_date = time.mktime(message.date.timetuple())
-           detecting = detect_user(user_id, chat_id, message, parsing_date)
-           if detecting:
-              return True
-           antispam_restrict_user(user_id, parsing_date)
-        if int(user_id) in SPAMMERS:
-           print("This user is spammer!")
-           return True
-        else:
-           return False
+    def spamfilters(_text, user_id, _chat_id):
+     if int(user_id) in SPAMMERS:
+        print("This user is a spammer!")
+        return True
+     else:
+        return False
 else:
    quit(1)
