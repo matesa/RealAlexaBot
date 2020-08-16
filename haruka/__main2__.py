0953de865d56332196adbf1cac49a89a00e36e6b
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-08-16 20:43:24 +0530

    Create __main2__.py

---
+++ haruka/__main2__.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot start point """
+
+from importlib import import_module
+
+import os
+
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+
+from haruka.__init__2 import LOGS, ubot
+
+
+INVALID_PH = '\nERROR: The phone no. entered is incorrect' \
+             '\n  Tip: Use country code (eg +44) along with num.' \
+             '\n       Recheck your phone number'
+
+try:
+    ubot.start()
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    exit(1)
+
+LOGS.info("Paperplane is alive! Test it by typing .alive on any chat."
+          " Should you need assistance, head to https://t.me/tgpaperplane")
+
+SEM_TEST = os.environ.get("SEMAPHORE", None)
+if SEM_TEST:
+    ubot.disconnect()
+else:
+    ubot.run_until_disconnected()
