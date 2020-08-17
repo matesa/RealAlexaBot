aab8e0b0ffbed417ef9e65411aba445f32d623d6
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-08-18 01:27:17 +0530

    Update misc.py

--- haruka/modules/misc.py
+++ haruka/modules/misc.py
@@ -2586,7 +2586,7 @@ def deEmojify(inputString: str) -> str:
 
 # Made by @AyushChatterjee
 
-@alexabot(incoming=True, pattern="/animated")
+@alexabot(pattern="/animated")
 async def waifu(animu):
   animus = [1, 3, 7, 9, 13, 22, 34, 35, 36, 37, 43, 44, 45, 52, 53, 55]
   sticcers = await animu.client.inline_query(
