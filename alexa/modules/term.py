b4498c2ad6edf2606024c7a6da36a0f17f1963e0
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-08-22 22:51:44 +0530

    Create term.py

---
+++ alexa/modules/term.py
@@ -0,0 +1,56 @@
+from alexa.events import register, OWNER ID
+import asyncio
+from getpass import getuser
+from os import remove
+from sys import executable
+
+@register(pattern="^/term(?: |$)(.*)")
+async def terminal_runner(term):
+ check = term.message.sender_id
+ checkint = int(check)
+ # print(checkint)
+ if int(check) != int(OWNER_ID):
+       return
+ else:
+    curruser = getuser()
+    command = term.pattern_match.group(1)
+    try:
+        from os import geteuid
+        uid = geteuid()
+    except ImportError:
+        uid = "This ain't it chief!"
+
+    if term.is_channel and not term.is_group:
+        await term.reply("`Term commands aren't permitted on channels!`")
+        return
+
+    if not command:
+        return
+
+    process = await asyncio.create_subprocess_shell(
+        command,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await process.communicate()
+    result = str(stdout.decode().strip()) \
+        + str(stderr.decode().strip())
+
+    if len(result) > 4096:
+        output = open("output.txt", "w+")
+        output.write(result)
+        output.close()
+        await term.client.send_file(
+            term.chat_id,
+            "output.txt",
+            reply_to=term.id,
+            caption="`Output too large, sending as file`",
+        )
+        remove("output.txt")
+        return
+
+    if uid == 0:
+        await term.reply("`" f"{curruser}:~# {command}" f"\n{result}" "`")
+    else:
+        await term.reply("`" f"{curruser}:~$ {command}" f"\n{result}" "`")
+
+ 
