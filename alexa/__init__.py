7d5c6efc657cc3f06df57723343fb35952d29497
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-08-24 08:12:09 +0530

    Update __init__.py

--- alexa/__init__.py
+++ alexa/__init__.py
@@ -776,7 +776,7 @@ if ENV:
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    HEROKU_MEMEZ = os.environ.get("HEROKU_MEMEZ", None)
+    HEROKU_MEMEZ = os.environ.get("HEROKU_MEMEZ", True)
     HEROKU_GIT_URL = os.environ.get("HEROKU_GIT_URL", None)
     GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", "RealAlexaBot")
     HEROKU_LINK = os.environ.get("HEROKU_GIT_URL")
