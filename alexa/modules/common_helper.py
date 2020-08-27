1f425d7f4edb37450942b0343df779410d226061
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-08-27 10:10:37 +0530

    Update common_helper.py

--- alexa/modules/common_helper.py
+++ alexa/modules/common_helper.py
@@ -725,7 +725,7 @@ __help__ = """
  - /markdownhelp: quick summary of how markdown works in telegram - can only be called in private chats
  - /smsbomb {country code without +} {phone no}: Take revenge upon your enemy by sending bulk messages 
  - /report <reason> | @admin: reply to a message to report it to admins(non-admin only)
- - /reports <on/off>: change report setting
+ - /reports <on/off>: change report setting (admin only)
 """
 
 __mod_name__ = "Helpers 🤗"
