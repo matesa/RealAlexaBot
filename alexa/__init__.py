03a59cfde7b7d76f6a7ede9af141f6926fa72877
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-08-27 11:03:56 +0530

    Update __init__.py

--- alexa/__init__.py
+++ alexa/__init__.py
@@ -776,13 +776,7 @@ if ENV:
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    HEROKU_MEMEZ = os.environ.get("HEROKU_MEMEZ", True)
-    HEROKU_GIT_URL = os.environ.get("HEROKU_GIT_URL", None)
-    GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", "RealAlexaBot")
-    HEROKU_LINK = os.environ.get("HEROKU_GIT_URL")
-    REPO_LINK = os.environ.get("UPSTREAM_REPO_URL")
-    UPSTREAM_REPO_URL = os.environ.get(
-        "UPSTREAM_REPO_URL", "https://github.com/Ayush1311/RealAlexaBot.git")
+    UPSTREAM_REPO_URL = os.environ.get("UPSTREAM_REPO_URL", "https://github.com/Ayush1311/RealAlexaBot.git")
     TEMPORARY_DATA = os.environ.get("TEMPORARY_DATA", None)
     SPAMMERS = list(SPAMMERS)
 
