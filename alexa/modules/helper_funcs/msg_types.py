33e9fc48a53f16926332ab2eb3c48a51d029a6cf
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-08-29 16:19:17 +0530

    Update msg_types.py

--- alexa/modules/helper_funcs/msg_types.py
+++ alexa/modules/helper_funcs/msg_types.py
@@ -669,7 +669,7 @@ from enum import IntEnum, unique
 
 from telegram import Message
 
-from skylee.modules.helper_funcs.string_handling import button_markdown_parser
+from alexa.modules.helper_funcs.string_handling import button_markdown_parser
 
 
 @unique
