5837f4d0c147a1617c1f3a4a191303b11e7157ac
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-08-29 12:18:15 +0530

    Update currency.py

--- alexa/modules/currency.py
+++ alexa/modules/currency.py
@@ -672,25 +672,27 @@ from alexa.modules.helper_funcs.chat_status import bot_admin, can_promote, user_
 
 from alexa import dispatcher, CASH_API_KEY
 
-@user_admin
+import requests
+from alexa import CASH_API_KEY, dispatcher
+from telegram import Update, ParseMode
+from telegram.ext import CallbackContext, CommandHandler, run_async
+
+
 @run_async
-def convert(bot: Bot, update: Update):
-    args = update.effective_message.text.split(" ", 3)
-    if len(args) > 1:
+def convert(update: Update, context: CallbackContext):
+    args = update.effective_message.text.split(" ")
 
-        orig_cur_amount = float(args[1])
-
+    if len(args) == 4:
         try:
-            orig_cur = args[2].upper()
-        except IndexError:
-            update.effective_message.reply_text("You forgot to mention the currency code.")
+            orig_cur_amount = float(args[1])
+
+        except ValueError:
+            update.effective_message.reply_text("Invalid Amount Of Currency")
             return
 
-        try:
-            new_cur = args[3].upper()
-        except IndexError:
-            update.effective_message.reply_text("You forgot to mention the currency code to convert into.")
-            return
+        orig_cur = args[2].upper()
+
+        new_cur = args[3].upper()
 
         request_url = (f"https://www.alphavantage.co/query"
                        f"?function=CURRENCY_EXCHANGE_RATE"
@@ -699,22 +701,35 @@ def convert(bot: Bot, update: Update):
                        f"&apikey={CASH_API_KEY}")
         response = requests.get(request_url).json()
         try:
-            current_rate = float(response['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+            current_rate = float(
+                response['Realtime Currency Exchange Rate']['5. Exchange Rate'])
         except KeyError:
-            update.effective_message.reply_text(f"Currency Not Supported.")
+            update.effective_message.reply_text("Currency Not Supported.")
             return
         new_cur_amount = round(orig_cur_amount * current_rate, 5)
-        update.effective_message.reply_text(f"{orig_cur_amount} {orig_cur} = {new_cur_amount} {new_cur}")
+        update.effective_message.reply_text(
+            f"{orig_cur_amount} {orig_cur} = {new_cur_amount} {new_cur}")
+
+    elif len(args) == 1:
+        update.effective_message.reply_text(
+            __help__, parse_mode=ParseMode.MARKDOWN)
+
     else:
-        update.effective_message.reply_text(__help__)
+        update.effective_message.reply_text(
+            f"*Invalid Args!!:* Required 3 But Passed {len(args) -1}",
+            parse_mode=ParseMode.MARKDOWN)
 
 
+
+CONVERTER_HANDLER = CommandHandler('cash', convert)
+
+dispatcher.add_handler(CONVERTER_HANDLER)
+
+
+__command_list__ = ["cash"]
+__handlers__ = [CONVERTER_HANDLER]
 __help__ = """
  - /cash : currency converter
  example syntax: /cash 1 USD INR
 """
 __mod_name__ = "Currency 💰"
-
-
-CONVERTER_HANDLER = CommandHandler('cash', convert)
-dispatcher.add_handler(CONVERTER_HANDLER)
