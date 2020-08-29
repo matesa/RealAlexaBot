56afda15c2279b8b5f466c03cd4324ba9fec6944
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-08-29 12:04:05 +0530

    Update chatbot.py

--- alexa/modules/chatbot.py
+++ alexa/modules/chatbot.py
@@ -665,85 +665,94 @@
 
 
 
+import html
 # AI module using Intellivoid's Coffeehouse API by @TheRealPhoenix
-from time import time, sleep
-from alexa.modules.helper_funcs.chat_status import bot_admin, can_promote, user_admin, can_pin, user_can_restrict, user_can_pin, user_can_change
-from coffeehouse.lydia import LydiaAI
+from time import sleep, time
+
+import alexa.modules.sql.chatbot_sql as sql
 from coffeehouse.api import API
 from coffeehouse.exception import CoffeeHouseError as CFError
-
-from telegram import Update, Bot
-from telegram.ext import CommandHandler, MessageHandler, Filters, run_async
-from telegram.error import BadRequest, Unauthorized, RetryAfter
-
-from alexa import dispatcher, LYDIA_API_KEY, OWNER_ID
-import alexa.modules.sql.chatbot_sql as sql
-from alexa.modules.helper_funcs.chat_status import user_admin
+from coffeehouse.lydia import LydiaAI
+from alexa import LYDIA_API_KEY, OWNER_ID, dispatcher
+from alexa.modules.helper_funcs.chat_status import user_admin, user_can_change
 from alexa.modules.helper_funcs.filters import CustomFilters
+from alexa.modules.log_channel import gloggable
+from telegram import Update
+from telegram.error import BadRequest, RetryAfter, Unauthorized
+from telegram.ext import (CallbackContext, CommandHandler, Filters,
+                          MessageHandler, run_async)
+from telegram.utils.helpers import mention_html
 
 CoffeeHouseAPI = API(LYDIA_API_KEY)
 api_client = LydiaAI(CoffeeHouseAPI)
 
 
 @run_async
-@user_admin
 @user_can_change
-def add_chat(_bot: Bot, update: Update):
+@gloggable
+def add_chat(update: Update, context: CallbackContext):
     global api_client
-    chat_id = update.effective_chat.id
-    if update.effective_message.chat.type == "private":
-      update.effective_message.reply_text("This command can only be used on group, not PM")
-      return 
+    chat = update.effective_chat
     msg = update.effective_message
-    is_chat = sql.is_chat(chat_id)
+    user = update.effective_user
+    is_chat = sql.is_chat(chat.id)
     if not is_chat:
         ses = api_client.create_session()
         ses_id = str(ses.id)
         expires = str(ses.expires)
-        sql.set_ses(chat_id, ses_id, expires)
+        sql.set_ses(chat.id, ses_id, expires)
         msg.reply_text("AI successfully enabled for this chat!")
+        message = (f"<b>{html.escape(chat.title)}:</b>\n"
+                   f"#AI_ENABLED\n"
+                   f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n")
+        return message
     else:
         msg.reply_text("AI is already enabled for this chat!")
+        return ""
 
 
 @run_async
-@user_admin
 @user_can_change
-def remove_chat(_bot: Bot, update: Update):
+@gloggable
+def remove_chat(update: Update, context: CallbackContext):
     msg = update.effective_message
-    chat_id = update.effective_chat.id
-    is_chat = sql.is_chat(chat_id)
-    if update.effective_message.chat.type == "private":
-      update.effective_message.reply_text("This command can only be used on group, not PM")
-      return 
+    chat = update.effective_chat
+    user = update.effective_user
+    is_chat = sql.is_chat(chat.id)
     if not is_chat:
         msg.reply_text("AI isn't enabled here in the first place!")
+        return ""
     else:
-        sql.rem_chat(chat_id)
+        sql.rem_chat(chat.id)
         msg.reply_text("AI disabled successfully!")
+        message = (f"<b>{html.escape(chat.title)}:</b>\n"
+                   f"#AI_DISABLED\n"
+                   f"<b>Admin:</b> {mention_html(user.id, user.first_name)}\n")
+        return message
 
 
-def check_message(bot: Bot, message):
+def check_message(context: CallbackContext, message):
     reply_msg = message.reply_to_message
-    if message.text.lower() == "alexa":
+    if message.text.lower() == "saitama":
         return True
     if reply_msg:
-        if reply_msg.from_user.id == bot.get_me().id:
+        if reply_msg.from_user.id == context.bot.get_me().id:
             return True
     else:
         return False
 
 
 @run_async
-def chatbot(bot: Bot, update: Update):
+def chatbot(update: Update, context: CallbackContext):
     global api_client
     msg = update.effective_message
     chat_id = update.effective_chat.id
     is_chat = sql.is_chat(chat_id)
+    bot = context.bot
     if not is_chat:
         return
     if msg.text and not msg.document:
-        if not check_message(bot, msg):
+        if not check_message(context, msg):
             return
         sesh, exp = sql.get_ses(chat_id)
         query = msg.text
@@ -762,17 +771,17 @@ def chatbot(bot: Bot, update: Update):
             sleep(0.3)
             msg.reply_text(rep, timeout=60)
         except CFError as e:
-            bot.send_message(
-                OWNER_ID, f"Chatbot error: {e} occurred in {chat_id}!")
+            bot.send_message(OWNER_ID,
+                             f"Chatbot error: {e} occurred in {chat_id}!")
 
 
 @run_async
-def list_chatbot(bot: Bot, update: Update):
+def list_chatbot_chats(update: Update, context: CallbackContext):
     chats = sql.get_all_chats()
     text = "<b>AI-Enabled Chats</b>\n"
     for chat in chats:
         try:
-            x = bot.get_chat(int(*chat))
+            x = context.bot.get_chat(int(*chat))
             name = x.title if x.title else x.first_name
             text += f"• <code>{name}</code>\n"
         except BadRequest:
@@ -786,12 +795,21 @@ def list_chatbot(bot: Bot, update: Update):
 
 ADD_CHAT_HANDLER = CommandHandler("autochat", add_chat)
 REMOVE_CHAT_HANDLER = CommandHandler("stopchat", remove_chat)
-CHATBOT_HANDLER = MessageHandler(Filters.text & (~Filters.regex(
-    r"^#[^\s]+") & ~Filters.regex(r"^!") & ~Filters.regex(r"^s\/")), chatbot)
-#  CHATBOTLIST_HANDLER = CommandHandler("listai", list_chatbot, filters=CustomFilters.dev_filter)
+CHATBOT_HANDLER = MessageHandler(
+    Filters.text & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!")
+                    & ~Filters.regex(r"^\/")), chatbot)
+LIST_CB_CHATS_HANDLER = CommandHandler(
+    "listaichats", list_chatbot_chats, filters=CustomFilters.dev_filter)
 # Filters for ignoring #note messages, !commands and sed.
 
 dispatcher.add_handler(ADD_CHAT_HANDLER)
 dispatcher.add_handler(REMOVE_CHAT_HANDLER)
 dispatcher.add_handler(CHATBOT_HANDLER)
-#  dispatcher.add_handler(CHATBOTLIST_HANDLER)
+dispatcher.add_handler(LIST_CB_CHATS_HANDLER)
+
+__mod_name__ = "Chatbot"
+__command_list__ = ["addchat", "rmchat", "listaichats"]
+__handlers__ = [
+    ADD_CHAT_HANDLER, REMOVE_CHAT_HANDLER, CHATBOT_HANDLER,
+    LIST_CB_CHATS_HANDLER
+]
