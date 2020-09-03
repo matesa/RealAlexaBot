20d3657dc2c355f5b6daffdde36fa8d7a7e7f300
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-09-03 22:42:03 +0530

    Update carbon.py

--- alexa/modules/carbon.py
+++ alexa/modules/carbon.py
@@ -725,9 +725,10 @@ async def carbon_api(e):
     if e.is_group:
         if not (await is_register_admin(e.input_chat, e.message.sender_id)):
             return
-        elif for ch in approved_users:
-           if event.chat_id == ch['id'] and event.from_id == ch['user']:  
-              pass
+        else:
+          for ch in approved_users:
+            if event.chat_id == ch['id'] and event.from_id == ch['user']:  
+               pass
     jj = "`Processing..`"
     gg = await e.reply(jj)
     CARBON = "https://carbon.now.sh/?bg=rgba(239%2C40%2C44%2C1)&t=one-light&wt=none&l=application%2Ftypescript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=143%25&si=false&es=2x&wm=false&code={code}"
