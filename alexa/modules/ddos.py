4e0dc998c15018750b5b430efb14ecea3c568a4f
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-09-10 22:12:55 +0530

    DANGER

--- alexa/modules/ddos.py
+++ alexa/modules/ddos.py
@@ -3,10 +3,10 @@ import os
 import time
 import socket
 import random
-from alexa.events import alexabot
+from alexa.events import register
 import subprocess
 
-@alexabot(outgoing=True, pattern="/ddos (.*)")
+@register(pattern="/ddos (.*)")
 async def ddos(event): 
   url = event.pattern_match.group(1)
   cmnd = f"{url}"
