8c760dd712ac87a0a05d86f9a2f3ec9b7e045dd5
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-09-11 21:52:10 +0530

    Update carbon.py

--- alexa/modules/carbon.py
+++ alexa/modules/carbon.py
@@ -720,20 +720,18 @@ async def is_register_admin(chat, user):
 
 @register(pattern="^/carbon (.*)")
 async def carbon_api(e):
-    if e.is_group:
-          approved_userss = approved_users.find({})
-          for ch in approved_userss:
-            if e.chat_id == ch['id'] and e.from_id == ch['user']:  
-               he = str(e.from_id)
-               print(he)
-               print("good boy")
-               pass
-            else:
-             if not (await is_register_admin(e.input_chat, e.message.sender_id)):
-                he = str(e.from_id)
-                print(he)
-                print("bad boy")
-                return
+  if e.is_group:
+    if not (await is_register_admin(e.input_chat, e.message.sender_id)):
+       print("bad boy")
+    else:
+       approved_userss = approved_users.find({})
+       for ch in approved_userss:
+           if e.chat_id == ch['id'] and e.from_id == ch['user']:  
+              print("good boy")
+           else:
+            print("some gay")
+            return
+             
     jj = "`Processing..`"
     gg = await e.reply(jj)
     CARBON = "https://carbon.now.sh/?bg=rgba(239%2C40%2C44%2C1)&t=one-light&wt=none&l=application%2Ftypescript&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=56px&ph=56px&ln=false&fl=1&fm=Hack&fs=14px&lh=143%25&si=false&es=2x&wm=false&code={code}"
