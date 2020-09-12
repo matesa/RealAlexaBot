0814022f25830905f93843af08d4e89be29a82e0
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-09-12 13:47:50 +0530

    Update chatbot_sql.py

--- alexa/modules/sql/chatbot_sql.py
+++ alexa/modules/sql/chatbot_sql.py
@@ -661,11 +661,8 @@
 #     <https://www.gnu.org/licenses/>.
 import threading
 
-from sqlalchemy import Column
-from sqlalchemy import String
-
-from alexa.modules.sql import BASE
-from alexa.modules.sql import SESSION
+from alexa.modules.sql import BASE, SESSION
+from sqlalchemy import Column, String
 
 
 class ChatbotChats(BASE):
