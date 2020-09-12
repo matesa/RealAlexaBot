4d52ccae15e1d6f0f798e0f8b5671b9a2c080762
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-09-12 13:39:39 +0530

    Update imdb.py

--- alexa/modules/imdb.py
+++ alexa/modules/imdb.py
@@ -677,6 +677,14 @@ from alexa.events import register
 
 langi = "en"
 
+from pymongo import MongoClient
+from alexa import MONGO_DB_URI
+from alexa.events import register
+
+client = MongoClient()
+client = MongoClient(MONGO_DB_URI)
+db = client['test']
+approved_users = db.approve
 
 async def is_register_admin(chat, user):
     if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
@@ -702,9 +710,18 @@ async def is_register_admin(chat, user):
 
 @register(pattern="^/imdb (.*)")
 async def imdb(e):
-    if e.is_group:
-        if not (await is_register_admin(e.input_chat, e.message.sender_id)):
-            return
+    approved_userss = approved_users.find({})
+    for ch in approved_userss: 
+        iid = ch['id']
+        userss = ch['user']
+
+    if (await is_register_admin(event.input_chat, event.message.sender_id)):
+       pass
+    elif event.chat_id == iid and event.from_id == userss:  
+       pass
+    else:
+       return
+
     try:
         movie_name = e.pattern_match.group(1)
         remove_space = movie_name.split(" ")
