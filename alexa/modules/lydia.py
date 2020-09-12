98d37f17ca8f00c93b17f7e6bf62497cd56af35b
Ayush Chatterjee <54566209+Ayush1311@users.noreply.github.com>
2020-09-12 13:49:04 +0530

    Update lydia.py

--- alexa/modules/lydia.py
+++ alexa/modules/lydia.py
@@ -663,7 +663,7 @@
 __help__ = """
 Powered by coffeehouse from `intellivoid.net`
 
- - /autochat: Type in reply to your message to activate. Activates AI mode in the chat the bot will give auto replies to anyone who tags the bot
- - /stopchat: Type in reply to your message to deactivate. Deactivates AI mode in the chat the bot will stop giving auto replies to anyone who tags the bot
+ - /autochat: Activates AI mode in the chat the bot will give auto replies to anyone who tags the bot
+ - /stopchat: Deactivates AI mode in the chat the bot will stop giving auto replies to anyone who tags the bot
 """
 __mod_name__ = "ChatBot 🤖"
